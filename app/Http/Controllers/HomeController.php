a4b8437de547a92e526552729019ebf13b0247e1
digilysolutions <digilysolutions@gmail.com>
2025-04-04 17:27:03 -0400

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,7 +33,7 @@ class HomeController extends Controller
     public function index()
     {
         $currency = $this->getCurrency();
-        $products = Product::allActivated();
+        $products = Product::all();
 
 
         $randomProducts = $products->shuffle()->toArray();
