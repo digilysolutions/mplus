d2b001233ae919567193974a650d29cbb1e82a49
digilysolutions <digilysolutions@gmail.com>
2025-05-31 01:30:36 -0400

    Update top-navbar.blade.php

--- resources/views/layouts/partials-front-rpg/top-navbar.blade.php
+++ resources/views/layouts/partials-front-rpg/top-navbar.blade.php
@@ -30,8 +30,9 @@
     <div class="row align-items-center py-3 px-xl-5">
         <div class="col-lg-3 d-none d-lg-block">
             <a href="/" class="text-decoration-none">
-               <span> <img class="img-fluid" height="10px" width="100px" src="{{asset('img/logo-rpg-solutions.png')}}"/></span>
-                  <img class="img-fluid w-50" src="{{asset('img/logo-solutions.png')}}"/>
+                <span> <img class="img-fluid" height="10px" width="100px"
+                        src="{{ asset('img/logo-rpg-solutions.png') }}" /></span>
+                <img class="img-fluid w-50" src="{{ asset('img/logo-solutions.png') }}" />
             </a>
 
         </div>
@@ -47,15 +48,33 @@
                 </div>
             </form>
         </div>
-        <div class="col-lg-3 col-6 text-right ">
-            <div class="btn border">
-                <a href="javascript:void(0);" class="text-secundary top-cart-info-count" id="item-count-phone">0
-                    productos: </a>
-                <a href="javascript:void(0);" class="top-cart-info-value" id="total-price-phone">$0.00</a>
 
-                <i class="fas fa-shopping-cart text-primary"></i>
+
+        <div class="col-lg-3 col-6 text-right top-cart-block cart-container">
+            <div class="btn border icono-shopping-cart" >
+                <span class="text-secundary top-cart-info-count" id="item-count">0
+                    productos</span>
+                <strong  class="top-cart-info-value" id="total-price">$0.00</strong>
+            </div>
+            <i  class="fas fa-shopping-cart text-primary icono-shopping-cart" style="cursor: pointer;"></i>
+            <div class="top-cart-content-wrapper cart-float hide" id="cart-content" style="display: none; ">
+                <div class="top-cart-content">
+                    <div class="slimScrollDiv" style="position: relative; overflow: hidden; ">
+                        <ul class="scroller" id="cart-items-list" style=" overflow: auto;">
+                            <!-- Los items del carrito se añadirán aquí desde jQuery -->
+                        </ul>
+                    </div>
+                    <div class="text-right">
+                        <a href="{{ route('cart.showCart') }}" class="btn btn-info">Ver Pedido</a>
+                        <a id="complete-purchase" href="{{ route('product.checkout', ['iddomicilio' => 0]) }}"
+                            class="btn btn-primary">Finalizar compra</a>
+                    </div>
+                </div>
             </div>
         </div>
+
+
+
     </div>
 </div>
 <!-- Topbar End -->
