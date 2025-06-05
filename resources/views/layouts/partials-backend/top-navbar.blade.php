03a403625b6f4a9e0f670eefd2772fb0f52b213c
digilysolutions <digilysolutions@gmail.com>
2025-06-05 08:20:08 -0400

    Update top-navbar.blade.php

--- resources/views/layouts/partials-backend/top-navbar.blade.php
+++ resources/views/layouts/partials-backend/top-navbar.blade.php
@@ -4,7 +4,7 @@
             <div class="iq-navbar-logo d-flex align-items-center justify-content-between">
                 <i class="ri-menu-line wrapper-menu"></i>
                 <a href="" class="header-logo">
-                    <img src="{{ asset('img/login/logo-mplus.png') }}" class="img-fluid rounded-normal" alt="logo">
+                    <img src="{{ asset('img/logo-rpg.png') }}" class="img-fluid rounded-normal" alt="logo">
 
 
 
