41005e72ac8765f6415a64f1777d1e8b5104f8c5
digilysolutions <digilysolutions@gmail.com>
2025-06-13 01:01:59 -0400

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,7 +30,7 @@
                             <div class="card-body">
                                 <div class="d-flex align-items-center mb-4 card-total-sale">
                                     <div class="icon iq-icon-box-2 bg-info-light">
-                                        <img src="{{ asset('img/product/1.png') }}" class="img-fluid" alt="image">
+                                        <img src="{{ asset('/admin/images/product/1.png') }}" class="img-fluid" alt="image">
                                     </div>
                                     <div>
                                         <p class="mb-2">Total ventas</p>
